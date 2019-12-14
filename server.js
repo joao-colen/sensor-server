e0eac5e268a4a1fe3f5dcde1858812320311b7cc
João Colen <joaovictorcolen17@gmail.com>
2019-12-13 22:20:46 -0300

    getting all the sensors

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 // create application/json parser
 var jsonParser = bodyParser.json();
 
+var mysql      = require('mysql');
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
@@ -14,4 +15,18 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-connection.end();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// getting all the sensors
+app.get('/sensors', function (req, res) {
+    connection.query('SELECT * from sensors' , function (error, results, fields) {
+      if (error) throw error;
+      res.send(results);
+    });
+});
+
+app.listen(3000);
